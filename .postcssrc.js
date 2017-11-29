e4d1a0d4024c3bbee44191cc39b20d3ebd20f81a
railgunxin <1307881527@qq.com>
2017-11-29 16:38:21 +0800

    添加全局样式

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,5 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
-
+// 浏览器兼容插件
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
