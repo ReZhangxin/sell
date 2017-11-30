bc632f593dc7f31b53ca00d5ff6ebb6e8aa627d6
railgunxin <1307881527@qq.com>
2017-11-30 17:28:38 +0800

    通过Axios获取数据

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-header></v-header>
-    <div class="tab">
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to='/goods'>商品</router-link>
       </div>
@@ -18,20 +18,52 @@
 
 <script>
 import header from '@/components/header/header.vue'
+import axios from 'axios'
+
+const ERR_OK = 0
+
 export default {
   name: 'app',
+  data () {
+    return {
+      seller: { }
+    }
+  },
+  created: function () {
+    axios.get('/api/seller')
+    .then(res => {
+      if (res.data.errno == ERR_OK) {
+        this.seller = res.data.data
+      }
+      console.log(res.data)
+      console.log(this.seller)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },
   components: {
     'v-header': header
   }
 }
 </script>
 
+<style>
+.tab{
+  background: #008080;
+}
+</style>
+
 <style lang="stylus">
+  @import './common/stylus/mixin.styl'
   .tab
     display: flex
     width: 100%
     height: 40px
     line-height: 40px
+    /* border-bottom: 1px solid */
+    border-1px(#ddd)
+    dddddd
     .tab-item
       flex: 1
       text-align: center
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    {{message}}
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" width="64" height="64" >
+        <div>
+          {{seller.name}}
+        </div>
+      </div>
+    </div>
+    <div class="bulletin-wrapper"></div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      message: '头部~'
+  props: {
+    seller: {
+      type: Object
     }
   }
 }
