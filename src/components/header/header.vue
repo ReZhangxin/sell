6bd44cbbf33c7cc9be0b63bc871cfa5ca737b7a2
railgunxin <1307881527@qq.com>
2017-12-01 17:40:03 +0800

    添加头部样式

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -14,8 +14,9 @@
         <div class="description">
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
-        <div class="supports">
-          {{seller.supports[0]['description']}}
+        <div v-if="seller.supports" class="supports">
+          <span class="icon"></span> 
+          <span>{{seller.supports[0].description}}</span>
         </div>
       </div>
     </div>
@@ -40,30 +41,46 @@ export default {
 </script>
 
 <style lang="stylus">
+@import '../../common/stylus/mixin'
 .header
   position relative
   blur: 10px
   background: rgba(7,17,27,.5)
   color rgb(255,255,255)
   overflow hidden
-  .avatar
-    margin 24px 16px 18px 24px
-    border-radius 2px
-    display inline-block
-    vertical-align middle
-  .content
-    display inline-block
-    vertical-align middle
-    .title
-      .name
-        font-size 16px
-        font-weight bold
-        line-height 18px
-    .description
-      font-size 12px
-      font-weight 200
-      line-height 12px
-      margin 8px 0 10px
+  .content-wrapper
+    padding 24px 12px 18px 24px
+    font-size 0
+    .avatar
+      border-radius 2px
+      display inline-block
+      vertical-align middle
+    .content
+      display inline-block
+      vertical-align middle
+      margin-left 16px
+      .title
+        margin 2px 0 8px 0
+        .brand
+          width 30px
+          height 18px
+          display inline-block
+          vertical-align top
+          bg-image('brand')
+          background-size 30px 18px
+          background-repeat no-repeat
+        .name
+          margin-left 6px
+          font-size 16px
+          font-weight bold
+          line-height 18px
+      .supports
+          line-height 12px
+          font-size 10px
+      .description
+        font-size 12px
+        line-height 12px
+        margin 8px 0 10px
 
 .background
   position absolute
@@ -79,7 +96,6 @@ export default {
   line-height 28px
   font-size 10px
   font-weight 200
-  color rgb(255,255,255)
   background-color rgba(7,17,27,.2)
   overflow hidden
   text-overflow ellipsis
