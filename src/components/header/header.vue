0c312cb466c879824e5a5fe37f58113b5666f0ca
railgunxin <1307881527@qq.com>
2017-12-07 13:43:00 +0800

    头部组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -40,7 +40,29 @@
         <div class="detail-wrapper clearfix">
           <div class="detail-main">
             <h1 class="name">{{seller.name}}</h1>
-            <star :size='48' :score="seller.score"></star>
+            <!-- 评分组件 -->
+            <div class="star-wrapper">
+              <star :size='48' :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li v-for="(item,index) in seller.supports" :key="item" class="supports-item">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
         <div class="detail-close"@click="detailShow = !detailShow">
@@ -197,6 +219,13 @@ export default {
     width 100%
     height 100%
     filter blur(10px)
+  .slide-fade-enter-active 
+    transition: all .5s ease
+  .slide-fade-leave-active 
+    transition: all .5s ease
+  .slide-fade-enter, .slide-fade-leave-to
+    transform: translateX(100%)
+    opacity: 0
   .detail
     position fixed
     z-index 100
@@ -207,6 +236,8 @@ export default {
     // 内容超出，出现滚动条
     overflow auto
     background rgba(7,17,27,.8)
+    // ios 背景模糊
+    backdrop-filter blur(10px)
     .detail-wrapper
       width 100%
       min-height 100%
@@ -218,7 +249,60 @@ export default {
           text-align center
           font-size 16px
           font-weight 700
-
+        .star-wrapper
+          margin-top 18px
+          padding 2px 0
+          text-align center
+        .title
+          display flex
+          width 80%
+          margin 28px auto 24px auto
+          .line
+            flex 1
+            position relative
+            top -6px
+            border-bottom 1px solid rgba(255,255,255,.2)
+          .text
+            padding 0 12px
+            font-weight 700
+            font-size 14px
+        .supports
+          width 80%
+          margin 0 auto
+          .supports-item
+            padding 0 12px
+            margin-bottom 12px
+            font-size 0
+            &:last-child
+              margin-bottom 0
+            .icon
+              display inline-block
+              width 16px
+              height 16px
+              vertical-align top
+              margin-right 6px
+              background-size 16px 16px
+              background-repeat no-repeat
+              &.decress
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              line-height 12px
+              font-size 12px
+        .bulletin
+          width 80%
+          margin 0 auto
+          .content
+            padding 0 12px
+            font-size 12px
+            line-height 24px
     .detail-close
       position relative
       width 32px
@@ -226,13 +310,4 @@ export default {
       margin -64px auto 0 auto
       clear both
       font-size 32px
-
-.slide-fade-enter-active 
-  transition: all .5s ease
-.slide-fade-leave-active 
-  transition: all .5s ease
-.slide-fade-enter, .slide-fade-leave-to
-  transform: translateX(100%)
-  opacity: 0
-
 </style>
