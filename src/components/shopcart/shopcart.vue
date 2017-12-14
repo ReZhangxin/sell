698d2b9c8fbd0afeb3e5569fc96f153c4095af36
railgunxin <1307881527@qq.com>
2017-12-14 17:33:34 +0800

    购物车组件

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,21 +3,60 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopp  ing_cart"></i>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price"></div>
-        <div class="des"></div>
+        <div class="price" :class="{'highlight':totalCount>0}">￥{{totalPrice}}元</div>
+        <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right"></div>
+      <div class="content-right">
+        <div class="pay">
+          ￥{{minPrice}}元起送
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  // 没有现实的话需要在app.vue传入props==》 <router-view :seller="seller" />
+  // 传递关系 router-view把seller传递给goods组件，然后goods把seller传递给shopcart组件
 
+  props: {
+    selectFoods: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    deliveryPrice: {
+      type: Number,
+      default: 0
+    },
+    minPrice: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    totalPrice () {
+      let total = 0
+      this.selectFoods.forEach((food) => {
+        total += food.price * food.count
+      })
+      return total
+    },
+    totalCount () {
+      let count = 0
+      this.selectFoods.forEach((food) => {
+        count += food.count
+      })
+      return count
+    }
+  }
 }
 </script>
 
@@ -32,6 +71,7 @@ export default {
   .content
     display flex
     background #141d27
+    color rgba(255,255,255,.4)
     font-size 0
     .content-left
       flex 1
@@ -53,16 +93,55 @@ export default {
           border-radius 50%
           background #2b343c
           text-align center
+          &.highlight
+            background rgb(0,160,220)
           .icon-shopping_cart
             line-height 44px
             width 100%
             font-size 24px
             color #80858a
+            &.highlight
+              color #fff
+      .num
+        position absolute
+        top 0
+        right 0
+        width 24px
+        height 16px
+        line-height 16px
+        text-align center
+        border-radius 16px
+        font-size 9px
+        font-weight 700
+        color #fff 
+        background rgb(240,20,20)
+        box-shadow 0 4px 8px 0 rgba(0,0,0,.4)
       .price
         display inline-block
+        vertical-align top
+        margin-top 12px
+        line-height 24px
+        padding-right 12px
+        box-sizing border-box
+        border-right 1px solid rgba(255,255,255,.1)
+        font-size 16px
+        font-weight 700
+        &.highlight
+          color #fff
       .desc
         display inline-block
+        vertical-align top
+        line-height 24px
+        margin 12px 0 0 12px
+        font-size 10px
     .content-right
       flex 0 0 105px
       width 105px
+      .pay
+        height 48px
+        text-align center
+        font-size 12px
+        line-height 48px
+        font-weight 700px
+        background #2b333b
 </style>
